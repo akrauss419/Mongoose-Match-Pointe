fe4f3c46c6749b125012e54927207dbc18589721
akrauss419 <akrauss419@gmail.com>
2022-10-28 11:18:14 -0700

    connect to database

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
